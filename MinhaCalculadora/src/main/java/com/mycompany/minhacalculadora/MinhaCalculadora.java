31208641e3aae03d171d91ceea821113b9d222ca
elizasanches <rfelipinga@gmail.com>
2024-11-12 14:32:43 -0300

    início do projeto

---
+++ MinhaCalculadora/src/main/java/com/mycompany/minhacalculadora/MinhaCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minhacalculadora;
+
+/**
+ *
+ * @author 14793856624
+ */
+public class MinhaCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
